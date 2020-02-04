8a0cac0509ee993ee3a86884b1bd43e58ad15b85
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2020-02-04 01:20:55 -0500

    Delete Assignment2_No1.sql

--- Assignment 2/Assignment2_No1.sql
+++
@@ -1,4 +0,0 @@
-SELECT CompanyName, DATE_FORMAT (ShippedDate, '%m /%d /%y ') AS ShippedDate, Freight 
-FROM orders
-JOIN customers ON orders.CustomerID = customers.CustomerID
-ORDER BY CompanyName ASC;
