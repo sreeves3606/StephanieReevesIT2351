93a83225a382861285e8da43709a4402d69cdba4
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2020-02-08 20:42:06 -0500

    Add files via upload

---
+++ Assignment3/Assignment3_No1.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers VALUES ('ADVAN', 'ADVANTAGE Motors', 'William Wilder', 'Sales Manager', 'Rua Xavier da Silveria 13', 'Rio de Janeiro', 'Copacabana', '22060', 'Brazil',
+'55-21-2545-6500', '55-21-8799-3200')
