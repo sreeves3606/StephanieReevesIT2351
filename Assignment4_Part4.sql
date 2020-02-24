18e8e3912950c9dec5973394d1189baec6a490bd
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2020-02-23 21:26:23 -0500

    Delete Assignment4_Part4.sql

--- Assignment4_Part4.sql
+++
@@ -1,6 +0,0 @@
-SELECT products.ProductName AS 'Product', ROUND(products.UnitPrice, 2) AS 'List Price'
-FROM products 
-WHERE products.UnitPrice >
- (SELECT AVG(products.UnitPrice)
-  FROM products)
-ORDER BY products.UnitPrice DESC
