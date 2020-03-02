31438dc4aeface90565549ca9be5b024eb7834cc
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2020-03-01 19:37:43 -0500

    Delete Assignment5_No3.sql

--- Assignment 5 Final/Assignment5_No3.sql
+++
@@ -1,6 +0,0 @@
-SELECT CompanyName AS 'Company Name',
-	ContactTitle AS 'Contact Title',
-	(CONCAT(LEFT(ContactTitle, 1), LOCATE(' ', RIGHT(ContactTitle, -1)))) AS 'Abbreviated Title',
-    SUBSTR(suppliers.phone, 2, 3) AS 'Phone', suppliers.country AS 'Country' 
-FROM suppliers
-ORDER BY CompanyName;
