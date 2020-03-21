f70ab2705de63eb7ea890b41dd2f3a29c6fd2bde
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2020-03-21 07:44:23 -0400

    Delete Lab 2_1.sql

--- Lab 2_1.sql
+++
@@ -1,8 +0,0 @@
-SELECT IF(GROUPING(suppliers.CompanyName), 'Grand Total', suppliers.CompanyName) AS 'CompanyName', 
-IF(GROUPING(products.ProductName), 'Total Product', products.ProductName) AS 'ProductName', 
-SUM(order_details.Quantity) AS 'Total Quantity/Product'
-FROM suppliers
-JOIN products ON suppliers.SupplierID = products.productID
-JOIN order_details ON products.ProductID = order_details.ProductID
-GROUP BY suppliers.CompanyName, products.ProductName WITH ROLLUP
-
