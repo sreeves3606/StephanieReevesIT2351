2fc6f44b396a84413d313f3e5ed4a53801a66e8d
Stephanie Reeves <35580478+sreeves3606@users.noreply.github.com>
2020-04-04 07:04:01 -0400

    Add files via upload

---
+++ Assignment 8/Assignment 8_2 Unique.sql
@@ -0,0 +1,25 @@
+USE StefReevesnorthwind;
+
+DROP PROCEDURE IF EXISTS insertProducts;
+
+DELIMITER //
+
+CREATE PROCEDURE insertProducts()
+BEGIN
+	DECLARE duplicate_ProductID INT DEFAULT FALSE;
+    
+	DECLARE CONTINUE HANDLER FOR 1062
+		SET duplicate_ProductID = TRUE;
+
+    INSERT INTO products(ProductID) VALUES (79);
+    
+    IF duplicate_ProductID = TRUE THEN
+		SELECT 'Requested row was not inserted - duplicate key found.' AS message;
+	ELSE
+		SELECT 'Success - 1 row was inserted.' AS message;
+	END IF;
+END//
+
+DELIMITER ;
+
+Call insertProducts();
